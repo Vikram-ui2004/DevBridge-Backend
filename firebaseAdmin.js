9a07f24c7d33625f585dab065611f47a4273fead
Vikram Nayak <nayakvikram038@gmail.com>
2025-07-02 12:37:34 +0530

    Add env

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,12 +1,23 @@
 // firebaseAdmin.js
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
-
+import dotenv from "dotenv";
+dotenv.config();
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      type: process.env.FB_TYPE,
+      projectId: process.env.FB_PROJECT_ID,
+      privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FB_CLIENT_EMAIL,
+      clientId: process.env.FB_CLIENT_ID,
+      authUri: process.env.FB_AUTH_URI,
+      tokenUri: process.env.FB_TOKEN_URI,
+      authProviderX509CertUrl: process.env.FB_AUTH_PROVIDER_X509_CERT_URL,
+      clientC509CertUrl: process.env.FB_CLIENT_X509_CERT_URL,
+    }),
   });
 }
+
 
 export const adminDB = admin.firestore();
